ddf1452389c71785a68c8fe75dffdcc2f3799f44
Akekapon Suebprom <akekapon.sueb@gmail.com>
2016-09-15 22:27:45 +0700

    Initial Commit

---
+++ newGit/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  newGit
+//
+//  Created by Akekapon Suebprom on 15/9/59.
+//  Copyright © พ.ศ. 2559 suebprom Tech. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
